a5cee45be1942f03c96e96f3677f7a8f6b48c269
emi2k3 <emilio.rodriguez@correo.ucu.edu.uy>
2026-02-26 17:01:03 -0300

    Día flojo hoy, no pude hacer que se graben los audios

--- bot.cjs
+++ bot.cjs
@@ -1,34 +1,41 @@
 const dotenv = require("dotenv");
-const fs = require('node:fs');
-const path = require('node:path');
-const { Client, GatewayIntentBits, REST, Routes} = require('discord.js');
+const fs = require("node:fs");
+const path = require("node:path");
+const {
+  Client,
+  GatewayIntentBits,
+  REST,
+  Routes,
+  Collection,
+} = require("discord.js");
 
-
-dotenv.config()
+dotenv.config();
 
 const client = new Client({
-    intents: [
-        GatewayIntentBits.Guilds, //Para que pueda interactuar en el sv
-        GatewayIntentBits.MessageContent, //Puede leer el contexto del servidor.
-        GatewayIntentBits.GuildVoiceStates, // Para que pueda entrar por voz.
-    ]
-}
-   
-);
+  intents: [
+    GatewayIntentBits.Guilds, //Para que pueda interactuar en el sv
+    GatewayIntentBits.MessageContent, //Puede leer el contexto del servidor.
+    GatewayIntentBits.GuildVoiceStates, // Para que pueda entrar por voz.
+  ],
+});
 
 // Comandos
 const commands = [];
-const foldersPath = path.join(__dirname, 'commands');
+client.commands = new Collection();
+const foldersPath = path.join(__dirname, "commands");
 const commandFolders = fs.readdirSync(foldersPath);
 
 for (const folder of commandFolders) {
-	const commandsPath = path.join(foldersPath, folder);
-	const commandFiles = fs.readdirSync(commandsPath).filter((file) => file.endsWith('.js'));
-	for (const file of commandFiles) {
-		const filePath = path.join(commandsPath, file);
-		const command = require(filePath);	
-		commands.push(command.data.toJSON()); 
-	}
+  const commandsPath = path.join(foldersPath, folder);
+  const commandFiles = fs
+    .readdirSync(commandsPath)
+    .filter((file) => file.endsWith(".js"));
+  for (const file of commandFiles) {
+    const filePath = path.join(commandsPath, file);
+    const command = require(filePath);
+    commands.push(command.data.toJSON());
+    client.commands.set(command.data.name, command);
+  }
 }
 
 // Construct and prepare an instance of the REST module
@@ -36,32 +43,42 @@ const rest = new REST().setToken(process.env.DISCORD_TOKEN);
 
 // Carga los comandos
 (async () => {
-	try {
-		console.log(`Started refreshing ${commands.length} application (/) commands.`);
+  try {
+    console.log(
+      `Started refreshing ${commands.length} application (/) commands.`,
+    );
 
-		// Edita para poner los comandos nuevos
-		const data = await rest.put(Routes.applicationGuildCommands(process.env.CLIENT_ID, process.env.SERVER_ID), { body: commands });
+    // Edita para poner los comandos nuevos
+    const data = await rest.put(
+      Routes.applicationGuildCommands(
+        process.env.CLIENT_ID,
+        process.env.SERVER_ID,
+      ),
+      { body: commands },
+    );
 
-		console.log(`Successfully reloaded ${data.length} application (/) commands.`);
-	} catch (error) {
-		
-		console.error(error);
-	}
+    console.log(
+      `Successfully reloaded ${data.length} application (/) commands.`,
+    );
+  } catch (error) {
+    console.error(error);
+  }
 })();
 
 //Eventos Iteraciones
-const eventsPath = path.join(__dirname, 'events');
-const eventFiles = fs.readdirSync(eventsPath).filter((file) => file.endsWith('.js'));
+const eventsPath = path.join(__dirname, "events");
+const eventFiles = fs
+  .readdirSync(eventsPath)
+  .filter((file) => file.endsWith(".js"));
 
 for (const file of eventFiles) {
-	const filePath = path.join(eventsPath, file);
-	const event = require(filePath);
-	if (event.once) {
-		client.once(event.name, (...args) => event.execute(...args));
-	} else {
-		client.on(event.name, (...args) => event.execute(...args)); // ...args?
-	}
+  const filePath = path.join(eventsPath, file);
+  const event = require(filePath);
+  if (event.once) {
+    client.once(event.name, (...args) => event.execute(...args));
+  } else {
+    client.on(event.name, (...args) => event.execute(...args)); // ...args?
+  }
 }
-
 
 client.login(process.env.DISCORD_TOKEN);
--- commands/utility/join.js
+++ commands/utility/join.js
@@ -1,24 +1,25 @@
 const { joinVoiceChannel } = require("@discordjs/voice");
-const { ChannelType,SlashCommandBuilder } = require("discord.js");
+const { ChannelType, SlashCommandBuilder } = require("discord.js");
 
 module.exports = {
-	data: new SlashCommandBuilder()
-        .setName('join')
-        .setDescription('Join the channel that the user is in')
-        .addChannelOption((option) => 
-            option
-        .setName('channel') 
+  data: new SlashCommandBuilder()
+    .setName("join")
+    .setDescription("Join the channel that the user is in")
+    .addChannelOption((option) =>
+      option
+        .setName("channel")
         .setDescription("the channel to join")
         .setRequired(true)
-        .addChannelTypes(ChannelType.GuildVoice)    
+        .addChannelTypes(ChannelType.GuildVoice),
     ),
-    async execute(interaction) {
-		const channel = interaction.options.getChannel('channel');
-        voiceConnection = joinVoiceChannel({
-            channelId: channel.id,
-            guildId: channel.guild.id,
-            adapterCreator: channel.guild.voiceAdapterCreator,
-        })
-        interaction.reply("Ahí voy wacho.");
-	},
+  async execute(interaction) {
+    const channel = interaction.options.getChannel("channel");
+    voiceConnection = joinVoiceChannel({
+      channelId: channel.id,
+      guildId: channel.guild.id,
+      adapterCreator: channel.guild.voiceAdapterCreator,
+      selfDeaf: false,
+    });
+    interaction.reply("Ahí voy wacho.");
+  },
 };
